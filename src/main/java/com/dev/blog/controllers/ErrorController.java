c32e8da31a449add8a6a2146f5f4462be2648b90
hungpb13 <hungpb13@gmail.com>
2025-04-28 22:47:53 +0700

    Delete Category Endpoint

--- src/main/java/com/dev/blog/controllers/ErrorController.java
+++ src/main/java/com/dev/blog/controllers/ErrorController.java
@@ -61,4 +61,13 @@ public class ErrorController {
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalStateException(IllegalStateException exception) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.CONFLICT.value())
+                .message(exception.getMessage())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
 }
