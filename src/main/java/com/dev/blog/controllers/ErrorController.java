3c1d93fa91f535e2ccb95ae38bd3d15b58d70f96
hungpb13 <hungpb13@gmail.com>
2025-04-29 20:11:03 +0700

    Loading Users

--- src/main/java/com/dev/blog/controllers/ErrorController.java
+++ src/main/java/com/dev/blog/controllers/ErrorController.java
@@ -4,6 +4,7 @@ import com.dev.blog.domain.dtos.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -69,5 +70,14 @@ public class ErrorController {
                 .message(exception.getMessage())
                 .build();
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiErrorResponse> handleBadCredentialsException(BadCredentialsException exception) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message("Incorrect username or password")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 }
